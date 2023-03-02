8fe5b30e600985494d559dc39e58c4a8c4596488
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-02 11:11:34 -0300

    03:11 - Herdando demais características

--- index.js
+++ index.js
@@ -10,11 +10,8 @@ Pessoa.prototype.morrer = function () {
     console.log(`A pessa ${this.nome} morreu`);
 }
 
-// const pessoa = new Pessoa('ariel', 29);
-// console.log(pessoa.__proto__);
-// console.log(Pessoa.prototype);
-
-function Funcionario(cargo) {
+function Funcionario(nome, idade, cargo) {
+    Pessoa.call(this, nome, idade);
     this.cargo = cargo;
 }
 
@@ -24,23 +21,19 @@ Funcionario.prototype.constructor = Funcionario;
 Funcionario.prototype.baterPonto = function () {
     console.log("bateu o ponto");
 }
-// const funcionario = new Funcionario('dev');
-// console.log(funcionario.__proto__.__proto__);
-// console.log(Funcionario.prototype);
 
-function Engenheiro(crea) {
+function Engenheiro(nome, idade, cargo, crea) {
+    Funcionario.call(this, nome, idade, cargo);
     this.crea = crea;
 }
 Engenheiro.prototype = Object.create(Funcionario.prototype);
 Engenheiro.prototype.constructor = Engenheiro;
 
 
-// const engenheiro = new Engenheiro(123123);
-// console.log(engenheiro.__proto__.__proto__);
-// console.log(Engenheiro.prototype);
+
+const engenheiro = new Engenheiro("ariel", 29, "dev", 123123);
 
 
-const engenheiro = new Engenheiro(123123);
+console.log(engenheiro);
 
-
-console.log(engenheiro.__proto__.__proto__.__proto__);
+engenheiro.nascer();
