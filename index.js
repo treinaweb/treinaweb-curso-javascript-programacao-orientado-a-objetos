517221dbb7f614e920ed0438af9038034c5323f8
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-02 11:16:02 -0300

    03:16 - Métodos assessores

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ function Pessoa(nome, sexo, idade, rg) {
 
     Object.defineProperty(this, 'sexo', {
         enumerable: true,
-        value: sexo,
-        writable: true,
+        // value: sexo,
+        // writable: true,
+        get: function () {
+            return sexo;
+        },
+        set: function (value) {
+            sexo = value;
+        },
         configurable: false,
     });
 
     Object.defineProperties(this, {
         idade: {
             enumerable: true,
-            value: idade,
-            writable: true,
+            // value: idade,
+            // writable: true,
             configurable: false,
+            get: () => idade,
+            set: function (value) {
+                idade = value;
+            }
         },
         rg: {
             enumerable: true,
@@ -26,5 +36,5 @@ function Pessoa(nome, sexo, idade, rg) {
 
 const pessoa = new Pessoa('ariel', 'M', 29, 123123);
 pessoa.sexo = "F";
-console.log(pessoa);
+console.log(pessoa.idade);
 
