beeaa27b73bd2a5ca48e5cb1c68e9b121cfe5d72
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-02 11:11:18 -0300

    03:10 - Trabalhando com prototypes na prática

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-function Counter() {
-    let _count = 0;
-
-    function _increment() {
-        _count++;
-    }
-
-    this.getCount = () => _count;
-
-    this.increment = function () {
-        _increment();
-    }
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
 }
 
+Pessoa.prototype.nascer = function () {
+    console.log(`A pessa ${this.nome} nasceu`);
+}
+Pessoa.prototype.morrer = function () {
+    console.log(`A pessa ${this.nome} morreu`);
+}
 
-const counter = new Counter();
+// const pessoa = new Pessoa('ariel', 29);
+// console.log(pessoa.__proto__);
+// console.log(Pessoa.prototype);
 
-counter.increment();
-counter.increment();
-counter.increment();
-counter.increment();
+function Funcionario(cargo) {
+    this.cargo = cargo;
+}
 
-console.log(counter.getCount());
+Funcionario.prototype = Object.create(Pessoa.prototype);
+Funcionario.prototype.constructor = Funcionario;
+
+Funcionario.prototype.baterPonto = function () {
+    console.log("bateu o ponto");
+}
+// const funcionario = new Funcionario('dev');
+// console.log(funcionario.__proto__.__proto__);
+// console.log(Funcionario.prototype);
+
+function Engenheiro(crea) {
+    this.crea = crea;
+}
+Engenheiro.prototype = Object.create(Funcionario.prototype);
+Engenheiro.prototype.constructor = Engenheiro;
+
+
+// const engenheiro = new Engenheiro(123123);
+// console.log(engenheiro.__proto__.__proto__);
+// console.log(Engenheiro.prototype);
+
+
+const engenheiro = new Engenheiro(123123);
+
+
+console.log(engenheiro.__proto__.__proto__.__proto__);
