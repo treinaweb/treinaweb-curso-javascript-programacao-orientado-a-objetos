837b07d7c16c8b185c0a0d145c8af913af157ff9
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-02 11:25:21 -0300

    04:21 - Herança

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
 class Pessoa {
-    #nome;
-    static pessoas = 0;
-    constructor(nome) {
-        this.#nome = nome;
-        Pessoa.pessoas++;
-    }
-
-    static pessoaModeu(valor) {
-        if (valor) {
-            Pessoa.pessoas -= valor;
-        }
+    nome;
+    idade;
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
     }
 
     androu() {
-        console.log(`O ${this.#nome} andou`);
-    }
-
-    get nome() {
-        return this.#nome;
-    }
-
-    set nome(value) {
-        this.#nome = value;
+        console.log(`A pessoa ${this.nome} andou`);
     }
 }
-const ariel = new Pessoa('Ariel');
-const pedro = new Pessoa('Pedro');
+
+class Aluno extends Pessoa {
+    nota;
+    constructor(nome, idade, nota) {
+        super(nome, idade);
+        this.nota = nota;
+    }
+
+    androu() {
+        console.log(`O aluno ${this.ariel} andou`)
+    }
+}
+
+class Paulo extends Aluno {
+
+    constructor(nome, idade, nota) {
+        super(nome, idade, nota);
+    }
+
+    androu() {
+        console.log(`O  ${this.nome} andou`)
+    }
+}
+
+const ariel = new Paulo('Ariel', 29, 10);
 console.log(ariel);
-new Pessoa('Pedro');
-new Pessoa('Pedro');
-new Pessoa('Pedro');
-new Pessoa('Pedro');
-new Pessoa('Pedro');
-
-Pessoa.pessoaModeu(3);
-
-new Pessoa('Pedro');
-new Pessoa('Pedro');
-new Pessoa('Pedro');
-new Pessoa('Pedro');
-
-Pessoa.pessoaModeu(1);
-console.log(Pessoa.pessoas);
-
+console.log(ariel.androu());
