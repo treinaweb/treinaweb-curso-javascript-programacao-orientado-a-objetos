7f7b0a3388b7c1533a3f1b35f27669788c5de83c
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-02 11:08:52 -0300

    02:04 - Cuidados ao utilizar o this

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-// const data = {
-//     addNome(nome) {
-//         this.nome = nome
-
-//         console.log(this);
-//     }
-// }
-
-// data.addNome('carlos');
+function Pessoa(nome) {
+    this.nome = nome;
 
 
-// function Pessoa(nome) {
-//     this.nome = nome;
+    console.log(this);
+}
 
-//     console.log(this);
-// }
-
-// new Pessoa("ariel");
-
-// new Pessoa("carlos");
-
-// new Pessoa("elton");
-
-console.log(this);
+Pessoa();
