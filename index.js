341ecc56a710ae7b84bd61323904e41b20d6d484
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-02 11:09:23 -0300

    02:06 - Usando o strict mode para evitar problemas com this

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+"use strict"
+
+var idade = 25
+
 function Pessoa(nome) {
-    if (!(this instanceof Pessoa)) {
-        return new Pessoa(nome);
-    }
 
 
     this.nome = nome;
     console.log(this instanceof Pessoa);
 }
 
-Pessoa();
+new Pessoa();
