c22e83b9e29d55a1e35d613f13016844e37668cb
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-02 11:12:14 -0300

    03:12 - Entendendo a cadeia de prototypes

--- index.js
+++ index.js
@@ -3,37 +3,11 @@ function Pessoa(nome, idade) {
     this.idade = idade;
 }
 
-Pessoa.prototype.nascer = function () {
-    console.log(`A pessa ${this.nome} nasceu`);
-}
-Pessoa.prototype.morrer = function () {
-    console.log(`A pessa ${this.nome} morreu`);
-}
-
-function Funcionario(nome, idade, cargo) {
-    Pessoa.call(this, nome, idade);
-    this.cargo = cargo;
-}
-
-Funcionario.prototype = Object.create(Pessoa.prototype);
-Funcionario.prototype.constructor = Funcionario;
-
-Funcionario.prototype.baterPonto = function () {
-    console.log("bateu o ponto");
-}
-
-function Engenheiro(nome, idade, cargo, crea) {
-    Funcionario.call(this, nome, idade, cargo);
-    this.crea = crea;
-}
-Engenheiro.prototype = Object.create(Funcionario.prototype);
-Engenheiro.prototype.constructor = Engenheiro;
+const p1 = new Pessoa("ariel", 29);
 
 
+const p2 = Object.create(p1);
+const p3 = new p1.constructor('elton', 49);
 
-const engenheiro = new Engenheiro("ariel", 29, "dev", 123123);
 
-
-console.log(engenheiro);
-
-engenheiro.nascer();
+console.log(p2);
