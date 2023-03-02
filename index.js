d371a6b364c35a38f65524961d75a9cb8dec5e5f
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-03-02 11:09:07 -0300

    02:05 - Verificando o tipo do construtor para evitar problemas com this

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function Pessoa(nome) {
+    if (!(this instanceof Pessoa)) {
+        return new Pessoa(nome);
+    }
+
+
     this.nome = nome;
-
-
-    console.log(this);
+    console.log(this instanceof Pessoa);
 }
 
 Pessoa();
